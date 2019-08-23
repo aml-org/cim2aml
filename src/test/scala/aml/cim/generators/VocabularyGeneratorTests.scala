0772054c6169ec2ba767a8dd5726804a8167dc7b
Antonio Garrote <antoniogarrote@gmail.com>
2019-08-23 15:37:35 -0700

    Restoring vocabulary testing

--- src/test/scala/aml/cim/generators/VocabularyGeneratorTests.scala
+++ src/test/scala/aml/cim/generators/VocabularyGeneratorTests.scala
@@ -1,11 +1,11 @@
 package aml.cim.generators
 
 import amf.client.AMF
-import amf.client.model.document.Dialect
+import amf.client.model.document.Vocabulary
 import amf.client.render.Aml10Renderer
 import amf.core.unsafe.PlatformSecrets
 import aml.cim.Utils
-import aml.cim.model.{ConceptualModel, SchemasModel}
+import aml.cim.model.ConceptualModel
 import org.scalatest.FunSuite
 
 class VocabularyGeneratorTests extends FunSuite with Utils with PlatformSecrets {
@@ -13,13 +13,13 @@ class VocabularyGeneratorTests extends FunSuite with Utils with PlatformSecrets 
   test("It should generate a AML vocabulary for each functional area in a conceptual model") {
     AMF.init().get()
 
-    val jsonld = loadExample("schemas")
-    val schemasModel = new SchemasModel(jsonld)
-    val functionalArea = schemasModel.functionalAreas.head
-    val dialect = new DialectGenerator(schemasModel, functionalArea).generate()
+    val jsonld = loadExample("concepts")
+    val conceptualModel = new ConceptualModel(jsonld)
+    val functionalArea = conceptualModel.functionalAreas.head
+    val vocabulary = new VocabularyGenerator(conceptualModel, functionalArea).generate()
 
-    val txt = new Aml10Renderer("application/yaml").generateString(new Dialect(dialect)).get().trim
-    val cs = platform.fs.syncFile("src/test/resources/schemas_example/schemas.yaml").read()
+    val txt = new Aml10Renderer("application/yaml").generateString(new Vocabulary(vocabulary)).get().trim
+    val cs = platform.fs.syncFile("src/test/resources/concepts_example/concepts.yaml").read()
     val target = cs.subSequence(0, cs.length()).toString
     assert(txt == target)
   }
